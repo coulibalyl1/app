b49fc97889f74069fde4ebd39499a8a12247b2c1
coulibalyl1 <coulibalymoussa695@gmail.com>
2026-03-28 14:44:56 -0400

    Reformat app.py for improved readability

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ sheet = client.open("Form_dat").sheet1  # ⚠️ mets le nom exact de ton fichie
 
 # 🔹 Interface utilisateur
 st.title("CL TRANSPORT POINTS")
-st.image("https://www.allrecipes.com/recipe/20144/banana-banana-bread/")
+st.image("https://www.allrecipes.com/recipe/20144/banana-banana-bread/", use_container_width=True)
 date_input = st.date_input("Date")
 Date = date_input.strftime("%d/%m/%Y")
 Abidjan=st.text_input("Abidjan")
